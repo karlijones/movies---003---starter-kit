20824bed51ade963ace7734ed088137b7ed2018f
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-06 14:03:09 -0500

    API request updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async ()=> {
     const response = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: '3cec4470',
-            s: 'avengers'
+            i: 'tt0848228'
         }
     });
 
