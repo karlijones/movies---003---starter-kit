594a45959e930af5c1b8c53d18331e25e5e681e3
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-06 22:01:19 -0500

    Awaiting async functions

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const fetchData = async (searchTerm)=> {
         }
     });
 
-    console.log(response.data);
+    return response.data.Search;
 }
 
 const input = document.querySelector('input');
 
 
-const onInput = debounce(event => {
-   fetchData(event.target.value); 
-});
+const onInput = async event => {
+   const movies = await fetchData(event.target.value); 
+   console.log(movies);
+};
 
 input.addEventListener('input', debounce(onInput, 500));
