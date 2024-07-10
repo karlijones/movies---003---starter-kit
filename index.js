430b33b82e53e7074feadc68645eaba4335e915b
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-09 22:33:24 -0500

    Making a follow up request

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const onInput = async event => {
         option.addEventListener('click', () => {
             dropdown.classList.remove('is-active');
             input.value = movie.Title;
+            onMovieSelect(movie);
         });
 
         resultsWrapper.appendChild(option);
@@ -63,3 +64,12 @@ document.addEventListener('click', event => {
         dropdown.classList.remove('is-active');
     }
 });
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params: {
+            apikey: '3cec4470',
+            i: movie.imdbID
+        }
+    });
+};
