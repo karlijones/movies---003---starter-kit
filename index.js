5b5d4e31e35ee72e54a49da040417874ff0bef3e
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-12 22:05:34 -0500

    Refactoring changing movie to item

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const fetchData = async (searchTerm)=> {
-    const response = await axios.get('http://www.omdbapi.com', {
-        params: {
-            apikey: '3cec4470',
-            s: searchTerm
-        }
-    });
-
-    if (response.data.Error) {
-       return []; 
-    }
-
-    return response.data.Search;
-};
 
 createAutoComplete({
     root: document.querySelector('.autocomplete')
@@ -27,7 +13,21 @@ createAutoComplete({
     },
     inputValue(movie) {
         return movie.Title;   
-    }
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com', {
+            params: {
+                apikey: '3cec4470',
+                s: searchTerm
+            }
+        });
+    
+        if (response.data.Error) {
+           return []; 
+        }
+    
+        return response.data.Search;
+    },
 });
 
 
