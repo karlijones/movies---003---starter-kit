cf56a39303b36b0a50515f90008e212a56558163
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-13 16:26:18 -0500

    Extracting stat values

--- index.js
+++ index.js
@@ -69,6 +69,11 @@ const runComparison = () => {
     console.log('Time for comparison');
 };
 const movieTemplate = (movieDetail) => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
     return `
         <article class="media">
             <figure class="media-left">
