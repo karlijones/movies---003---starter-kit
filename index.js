b41739a4f32cd3383cbf49af315c137114d358a1
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-13 14:41:22 -0500

    refactoring left and right autocomplete functions

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete')
+const autoCompleteConfig = {
     renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
@@ -28,6 +26,15 @@ createAutoComplete({
     
         return response.data.Search;
     },
+};
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete')
+});
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete')
 });
 
 
